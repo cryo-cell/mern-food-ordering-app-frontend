1d553258ed2b638ee824d85ff19d54f62dbcd01a
cryo-cell <cryobabbi@gmail.com>
2025-01-09 03:58:50 -0500

    manage restaurant form - api

--- src/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm.tsx
+++ src/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -8,55 +8,83 @@ import MenuSection from "./MenuSection";
 import ImageSection from "./ImageSection";
 import LoadingButton from "@/components/LoadingButton";
 import { Button } from "@/components/ui/button";
+import { Restaurant } from "@/types";
+import { useEffect } from "react";
 
-const formSchema = z.object({
+const formSchema = z
+  .object({
     restaurantName: z.string({
-        required_error: "Restaurant name is required.",
+      required_error: "restuarant name is required",
     }),
     city: z.string({
-        required_error: "City name is required.",
+      required_error: "city is required",
     }),
     country: z.string({
-        required_error: "Country name is required.",
+      required_error: "country is required",
     }),
     deliveryPrice: z.coerce.number({
-        required_error: "Delivery price is required.",
-        invalid_type_error: "Delivery price must be a number."
+      required_error: "delivery price is required",
+      invalid_type_error: "must be a valid number",
     }),
     estimatedDeliveryTime: z.coerce.number({
-        required_error: "Delivery time is required",
-        invalid_type_error: "Delivery time must be a number."
-
+      required_error: "estimated delivery time is required",
+      invalid_type_error: "must be a valid number",
     }),
-    /*cuisines: z.array(z.string()).nonempty({
-        message: "please select at least on item",
-    }),*/
-    menuItems: z.array(z.object({
+    menuItems: z.array(
+      z.object({
         name: z.string().min(1, "name is required"),
-        price: z.coerce.number().min(1, "price is required")
-    })),
-    imageFile: z.instanceof(File, {message: "image is required"})
+        price: z.coerce.number().min(1, "price is required"),
+      })
+    ),
+    imageUrl: z.string().optional(),
+    imageFile: z.instanceof(File, { message: "image is required" }).optional(),
+  })
+  .refine((data) => data.imageUrl || data.imageFile, {
+    message: "Either image URL or image File must be provided",
+    path: ["imageFile"],
+  });
 
-    
-})
-
-type restaurantFormData = z.infer<typeof formSchema>
+type RestaurantFormData = z.infer<typeof formSchema>;
 
 type Props = {
-  onSave: (restaurantFormData: FormData)=> void
-    isLoading: boolean;
-  
-}
+  restaurant?: Restaurant;
+  onSave: (restaurantFormData: FormData) => void;
+  isLoading: boolean;
+};
 
-const ManageRestaurantForm = ({onSave, isLoading}: Props) => {
-  const form = useForm<restaurantFormData>({
+const ManageRestaurantForm = ({ onSave, isLoading, restaurant }: Props) => {
+  const form = useForm<RestaurantFormData>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-        menuItems: [{name: "", price: 0}],
-    }
-  })
+      menuItems: [{ name: "", price: 0 }],
+    },
+  });
 
-  const onSubmit = (formDataJson: restaurantFormData) => {
+  useEffect(() => {
+    if (!restaurant) {
+      return;
+    }
+
+    // price lowest domination of 100 = 100pence == 1GBP
+    const deliveryPriceFormatted = parseInt(
+      (restaurant.deliveryPrice / 100).toFixed(2)
+    );
+
+    const menuItemsFormatted = restaurant.menuItems.map((item) => ({
+      ...item,
+      price: parseInt((item.price / 100).toFixed(2)),
+    }));
+
+    const updatedRestaurant = {
+      ...restaurant,
+      deliveryPrice: deliveryPriceFormatted,
+      menuItems: menuItemsFormatted,
+    };
+
+    form.reset(updatedRestaurant);
+  }, [form, restaurant]);
+
+  const onSubmit = (formDataJson: RestaurantFormData) => {
     const formData = new FormData();
 
     formData.append("restaurantName", formDataJson.restaurantName);
@@ -71,7 +99,7 @@ const ManageRestaurantForm = ({onSave, isLoading}: Props) => {
       "estimatedDeliveryTime",
       formDataJson.estimatedDeliveryTime.toString()
     );
-    formDataJson.menuItems.forEach((menuItem: { name: string | Blob; price: number; }, index: number) => {
+    formDataJson.menuItems.forEach((menuItem, index) => {
       formData.append(`menuItems[${index}][name]`, menuItem.name);
       formData.append(
         `menuItems[${index}][price]`,
@@ -85,22 +113,23 @@ const ManageRestaurantForm = ({onSave, isLoading}: Props) => {
 
     onSave(formData);
   };
+
   return (
     <Form {...form}>
-        <form 
+      <form
         onSubmit={form.handleSubmit(onSubmit)}
         className="space-y-8 bg-gray-50 p-10 rounded-lg"
-        >
-            <DetailsSection/>
-            <Separator/>
-            <MenuSection />
-            <Separator />
-            <ImageSection />
-            {isLoading ? <LoadingButton />: <Button type="submit">Submit</Button>}
-        </form>
-
+      >
+        <DetailsSection />
+        <Separator />
+        <Separator />
+        <MenuSection />
+        <Separator />
+        <ImageSection />
+        {isLoading ? <LoadingButton /> : <Button type="submit">Submit</Button>}
+      </form>
     </Form>
-  )
-}
+  );
+};
 
 export default ManageRestaurantForm;
